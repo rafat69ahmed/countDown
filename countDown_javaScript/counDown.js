c3db560f58e32e29cc9374e7dd37a3d177d0bce9
Rafat Ahmed <rafat69ahmed@gmail.com>
2015-11-05 17:01:37 +0600

    count down

---
+++ countDown_javaScript/counDown.js
@@ -0,0 +1,38 @@
+function loaded()
+{
+
+var futureDate =  new Date("Dec 07 2015 12:40:16 GMT+0600 (BDT)");
+var date = new Date();
+var presentTime = date.getTime();
+
+var timelapse = futureDate - presentTime;
+console.log(timelapse);
+
+var days = timelapse / (3600*1000*24);
+console.log(days);
+var floor_days = Math.floor(days);
+console.log(floor_days);
+document.getElementById('date').innerHTML = floor_days;
+
+var hours = (days - floor_days) * 24;
+console.log(hours);
+var floor_hours = Math.floor(hours);
+console.log(floor_hours);
+document.getElementById('hour').innerHTML = floor_hours;
+
+var mins = (hours - floor_hours) * 60;
+console.log(mins);
+var floor_mins = Math.floor(mins);
+console.log(floor_mins);
+document.getElementById('min').innerHTML = floor_mins;
+
+var sec = (mins - floor_mins) * 60;
+console.log(sec);
+var floor_sec = Math.floor(sec);
+console.log(floor_sec);
+
+(floor_sec < 10 ? document.getElementById('sec').innerHTML = "0" + floor_sec : document.getElementById('sec').innerHTML = floor_sec);
+
+
+}
+setInterval(loaded,1000);
